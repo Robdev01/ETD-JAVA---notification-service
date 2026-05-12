20ce4dc46e20e121a80bf94d0394e625f9439186
Robsondev <robsondev01@outlook.com>
2026-05-11 23:09:00 -0300

    adding consumer

--- notification-service/src/main/java/com/skillbase/notification_service/consumer/EmailConsumer.java
+++ notification-service/src/main/java/com/skillbase/notification_service/consumer/EmailConsumer.java
@@ -1,4 +1,50 @@
 package com.skillbase.notification_service.consumer;
 
+import com.skillbase.notification_service.config.RabbitMQConstants;
+import com.skillbase.notification_service.dto.EmailRequestDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
 public class EmailConsumer {
+
+    @RabbitListener(
+            queues = RabbitMQConstants.FILA_EMAIL
+    )
+    public void consumirMensagem(
+            EmailRequestDTO emailRequestDTO
+    ) {
+
+        log.info(
+                "MENSAGEM RECEBIDA DA FILA"
+        );
+
+        log.info(
+                "NOME: {}",
+                emailRequestDTO.nome()
+        );
+
+        log.info(
+                "EMAIL: {}",
+                emailRequestDTO.email()
+        );
+
+        log.info(
+                "ASSUNTO: {}",
+                emailRequestDTO.assunto()
+        );
+
+        log.info(
+                "MENSAGEM: {}",
+                emailRequestDTO.mensagem()
+        );
+
+        log.info(
+                "PROCESSAMENTO FINALIZADO"
+        );
+    }
+
 }
